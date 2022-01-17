86eb3468795c7f13fce555ba637c1e6e95493be0
Surkus <v.surkus@me.com>
2022-01-17 11:59:58 +0200

    Initial commit

---
+++ cs11_paskaita_Structs-NUnits/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace cs11_paskaita_Structs_NUnits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("cs11_PASKAITA!");
+
+        }
+    }
+}
+
